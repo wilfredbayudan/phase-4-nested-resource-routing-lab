91acbef202cdef3447b254a7bac510354da57d1b
Wilfred <wilfredbayudan@gmail.com>
2022-01-11 04:54:33 -1000

    commit tree 4c932458686b3e1bf08f389af7da9be4c8c1f139

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,38 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find_by(id: params[:user_id])
+      if user
+        items = user.items
+      else
+        return render json: { error: "User not found" }, status: :not_found
+      end
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find_by(id: params[:id])
+    if item
+      render json: item
+    else
+      render json: { error: "Item not found" }, status: :not_found
+    end
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
   end
 
 end
